7f684e653ff1d8f93d6896dad34e3a4398a4316c
CML90 <caitlincebu@hotmail.com>
2025-04-18 22:29:52 +0800

    feat: added section 8 and assets

---
+++ src/components/Section8/Section8.tsx
@@ -0,0 +1,34 @@
+import { ReactNode } from "react";
+
+export default function Section8(): ReactNode {
+    return(
+        <section className="bg-[url(/img/Section8/eighth_background.webp)] w-screen h-[1113px] bg-cover pt-[137px]">
+            <div className="container">
+                <div className="flex justify-between items-center">
+                    <h1 className="!text-[56px] leading-[54.88px] !mb-0">
+                        <span className="text-white">
+                            Let's&nbsp;
+                        </span>
+                        <span className="text-[#C2B8FF]">
+                            Build Together
+                        </span>
+                    </h1>
+                    <p className="!text-[20px] leading-[25.6px] !mb-0 capitalize">
+                        We’re proud to be part of the Cardano community<br />
+                        —constantly building, collaborating, and <br />
+                        connecting with others who share the vision.
+                    </p>
+                </div>
+
+                <div className="grid grid-cols-12 gap-[12px] mt-[65px]">
+                    <img src="/img/Section8/frederik_gregaard.webp" alt="Frederik Gregaard" className="col-span-6 w-full h-[371px] rounded-[24px] object-cover object-[center_-20px]"/>
+                    <img src="/img/Section8/philip_disarro_jonathan_rodriguez.webp" alt="Phil and Jonathan " className="col-span-6 w-full h-[371px] rounded-[24px] object-cover object-[center_-100px]"/>
+
+                    <img src="/img/Section8/hongjing_k.webp" alt="Jingles" className="col-span-4 w-full h-[371px] rounded-[24px] object-cover object-[center_-20px]"/>
+                    <img src="/img/Section8/cardano_girls.webp" alt="Lily and Lenna" className="col-span-4 w-full h-[371px] rounded-[24px] object-cover object-[center_-20px]"/>
+                    <img src="/img/Section8/diego_mac.webp" alt="Diego" className="col-span-4 w-full h-[371px] rounded-[24px] object-cover object-[center_-20px]"/>
+                </div>
+            </div>
+        </section>
+    )
+};

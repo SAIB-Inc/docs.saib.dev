d7037cc471ac9ec6069d00502c73e4c18f0457fb
CML90 <caitlincebu@hotmail.com>
2025-04-19 00:56:34 +0800

    style: fix container class and adjust indentation

--- src/components/Section3/Section3.tsx
+++ src/components/Section3/Section3.tsx
@@ -4,16 +4,14 @@ import { ReactNode } from "react";
 export default function Section3(): ReactNode {
     return (
         <section className="bg-darkbg bg-lines h-[1088.02px] flex flex-col items-center bg-[url(/img/Section3/background.webp)] w-screen bg-cover">
-            <div className="w-[1312px] flex flex-col justify-center items-center">
+            <div className="container flex flex-col justify-center items-center">
 
                 <div className="text-center h-[64px] flex flex-row items-center mb-[178px] mt-[36px]">
                     <div>
-                        <img alt="Buriza"
-                            src="/img/Section3/buriza.webp" />
+                        <img alt="Buriza" src="/img/Section3/buriza.webp" />
                     </div>
                     <div className="ml-[80px]">
-                        <img
-                            src="/img/Section3/arrow.webp" />
+                        <img src="/img/Section3/arrow.webp" />
                     </div>
 
                     <div className="ml-[40px] mr-[40px] flex items-center">
@@ -21,25 +19,29 @@ export default function Section3(): ReactNode {
                     </div>
 
                     <div>
-                        <img
-                            src="/img/Section3/arrow.webp" className="scale-x-[-1] mr-[80px]" />
+                        <img src="/img/Section3/arrow.webp" className="scale-x-[-1] mr-[80px]" />
                     </div>
                     <div>
-                        <img
-                            src='/img/Section3/levvy.webp' />
+                        <img src='/img/Section3/levvy.webp' />
                     </div>
                 </div>
 
                 <div>
                     <div className="mb-[64.94px] flex flex-row">
-                        <div className="w-[755px] h-[108px] flex flex-row items-center justify-center">
-                            <h1><span className="text-[#C2B8FF] text-[56px] font-semibold leading-[53.76px]">Argus</span><span className="text-white text-[56px] font-semibold leading-[53.76px]"> - The .NET Cardano Indexing Framework</span></h1>
+                        <div className="w-full h-[108px] flex flex-row items-center justify-center">
+                            <h1>
+                                <span className="text-[#C2B8FF] text-[56px] font-semibold leading-[53.76px]">Argus</span>
+                                <span className="text-white text-[56px] font-semibold leading-[53.76px]"> - The .NET Cardano Indexing Framework</span>
+                            </h1>
                         </div>
 
                         <div className="w-[557px] flex flex-col items-end">
                             <div className="w-[304.95px] h-[60px] mt-[2px] mb-[20.06px] flex justify-end" >
-                                <p className="text-white text-[16px] font-normal leading-[20.48px] ">Seamlessly Connecting Cardano And .NET For A Fast, Productive Developer Experience</p>
+                                <p className="text-white text-[16px] font-normal leading-[20.48px] ">
+                                    Seamlessly Connecting Cardano And .NET For A Fast, Productive Developer Experience
+                                </p>
                             </div>
+
                             <div>
                                 <Button variant="contained" 
                                     sx={{
@@ -47,18 +49,18 @@ export default function Section3(): ReactNode {
                                         justifyContent: 'space-between',
                                         alignItems: 'center',
                                         width: '305px',
-                                        px: 0,
+                                        px: 0
                                     }}
                                 >
                                     <p className="text-[16px] leading-[24px] font-normal !ml-[24px]">Learn More</p>
-                                    <img src="/img/Section3/button.webp" className="w-[24px] h-[24px] mr-[16px]"></img>
+                                    <img src="/img/Section3/button.webp" className="w-[24px] h-[24px] mr-[16px]"/>
                                 </Button>
                             </div>
                         </div>
                     </div>
 
                     <div>
-                        <img src='/img/Section3/code.webp' ></img>
+                        <img src='/img/Section3/code.webp' />
                     </div>
                 </div>
             </div>

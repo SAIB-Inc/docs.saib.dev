a25a8a86d698c8475205624ff0e13d1d636e81a3
Clark Alesna <clark_alesna@hotmail.com>
2025-04-23 13:38:32 +0800

    feat: added fourth section to homepage (#11)
    * feat: added fourth section to homepage
    
    * Add Catalyst SVG icon to the project
    
    - Introduced a new SVG file for the Catalyst icon located in the src/icons directory.
    - The SVG includes detailed path definitions for rendering the icon in white.
    - Added a clipping path to ensure proper rendering within the defined view box.
    
    ---------
    
    Co-authored-by: CML90 <caitlincebu@hotmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import Button from '@mui/material/Button';
 import Section1 from '../components/Section1/Section1';
 import Section3 from '../components/Section3/Section3';
 import Section2 from '../components/Section2/Section2';
+import Section4 from '../components/Section4/Section4';
 // function HomepageHeader() {
 //   const {siteConfig} = useDocusaurusContext();
 //   return (
@@ -37,6 +38,7 @@ export default function Home(): ReactNode {
       <Section1 />
       <Section2 />
       <Section3 />
+      <Section4 />
     </Layout>
   );
 }

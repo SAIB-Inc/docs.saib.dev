b3f02d0749465b7b67bce6ee77112634d68901eb
Mercurial <clark_alesna@hotmail.com>
2025-04-23 13:44:33 +0800

    feat: add Section5 to homepage layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ import Section1 from '../components/Section1/Section1';
 import Section3 from '../components/Section3/Section3';
 import Section2 from '../components/Section2/Section2';
 import Section4 from '../components/Section4/Section4';
+import Section5 from '../components/Section5/Section5';
 // function HomepageHeader() {
 //   const {siteConfig} = useDocusaurusContext();
 //   return (
@@ -39,6 +40,7 @@ export default function Home(): ReactNode {
       <Section2 />
       <Section3 />
       <Section4 />
+      <Section5 />
     </Layout>
   );
 }

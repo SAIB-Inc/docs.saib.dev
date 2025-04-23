5c47812fd8ad4e452155c39b6dfd8ceb98d38d65
CML90 <caitlincebu@hotmail.com>
2025-04-23 17:41:50 +0700

    feat: replace deprecated image formats with SVGs for Section5

--- src/components/Section5/Section5.tsx
+++ src/components/Section5/Section5.tsx
@@ -1,7 +1,8 @@
+import { Paper } from "@mui/material";
 import { ReactNode } from "react";
 
 export default function Section5(): ReactNode {
-    return(
+    return (
         <section className="bg-[url(/img/Section5/fifth_background.webp)] bg-cover w-screen h-[1203px]">
             <div className="container flex flex-col items-center !pt-[227px] !pb-[134px]">
                 <div>
@@ -11,33 +12,72 @@ export default function Section5(): ReactNode {
                     </h1>
                 </div>
 
-                <div className="flex gap-[16px] mt-[80px]">
-                    <div className="w-[636px] h-[654px] bg-[url(/img/Section5/power.webp)] bg-cover pt-[80px] pl-[49px]" >
-                        <h2 className="!text-[48px] font-bold !leading-[32px] !text-[#C2B8FF] !mb-[48px]">Power</h2>
-                        <p className="!text-[24px] !leading-[32px]">
-                            Harness the potential of C# with<br />
-                            robust tools such as LINQ, ASP.NET,<br />
-                            and Entity Framework.
-                        </p>
-                    </div>
-                    
+                <div className="flex gap-[16px] mt-[40px]">
+                    <Paper sx={{
+                        width: "636px",
+                        height: "654px",
+                        backgroundColor: '#24222D',
+                        paddingTop: '80px',
+                        paddingLeft: '49px',
+                        paddingRight: '45.74px'
+                    }}>
+                        <div className="flex flex-col">
+                            <h2 className="!text-[48px] font-bold !leading-[32px] !text-[#C2B8FF] !mb-[48px]">Power</h2>
+                            <p className="!text-[24px] !leading-[32px] !mb-[9.10px]">
+                                Harness the potential of C# with<br />
+                                robust tools such as LINQ, ASP.NET,<br />
+                                and Entity Framework.
+                            </p>
+                            <img src="/img/Section5/power.svg" alt="power" className="self-end" />
+                        </div>
+                    </Paper>
                     <div className="flex flex-col gap-[16px]">
-                        <div className="w-[636px] h-[319px] bg-[url(/img/Section5/efficiency.webp)] bg-cover pt-[80px] pl-[48px]">
-                            <h2 className="!text-[48px] font-bold !leading-[32px] !text-[#C2B8FF] !mb-[48px]">Efficiency</h2>
-                            <p className="!text-[20px] !leading-28px]">
-                                Experience streamlined developer<br />
-                                experience for smooth dApp<br />
-                                development and maintenance.
-                            </p>
-                        </div>
-                        <div className="w-[636px] h-[319px] bg-[url(/img/Section5/customizability.webp)] bg-cover pt-[80px] pl-[48px]">
-                            <h2 className="!text-[48px] font-bold !leading-[32px] !text-[#C2B8FF] !mb-[48px]">Customizability</h2>
-                            <p className="!text-[20px] !leading-28px]">
-                                Leverage built-in reducers or create<br />
-                                custom ones tailored to your<br />
-                                requirements.
-                            </p>
-                        </div>
+                        <Paper sx={{
+                            width: "636px",
+                            height: "319px",
+                            paddingTop: '80px',
+                            paddingLeft: '48px',
+                            paddingRight: '40.5px',
+                            paddingBottom: '40px',
+                            backgroundColor: '#24222D',
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                        }}>
+                            <div>
+                                <h2 className="!text-[48px] font-bold !leading-[32px] !text-[#C2B8FF] !mb-[48px]">Efficiency</h2>
+                                <p className="!text-[20px] !leading-28px]">
+                                    Experience streamlined developer<br />
+                                    experience for smooth dApp<br />
+                                    development and maintenance.
+                                </p>
+                            </div>
+                            <div className="flex flex-col justify-end">
+                                <img src="/img/Section5/efficiency.svg" alt="efficiency" />
+                            </div>
+                        </Paper>
+                        <Paper sx={{
+                            backgroundColor: '#24222D',
+                            width: "636px",
+                            height: "319px",
+                            paddingTop: '80px',
+                            paddingLeft: '48px',
+                            paddingRight: '40px',
+                            paddingBottom: '40px',
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                        }}>
+                            <div>
+                                <h2 className="!text-[48px] font-bold !leading-[32px] !text-[#C2B8FF] !mb-[48px]">Customizability</h2>
+                                <p className="!text-[20px] !leading-28px]">
+                                    Leverage built-in reducers or create<br />
+                                    custom ones tailored to your<br />
+                                    requirements.
+                                </p>
+                            </div>
+                            <div className="flex flex-col justify-end">
+                                <img src="/img/Section5/customizability.svg" alt="customizability" />
+                            </div>
+                        </Paper>
                     </div>
                 </div>
             </div>

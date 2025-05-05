6a3c9b1d7867ee6423226ae56a83e99b6f1ad1ab
CML90 <caitlincebu@hotmail.com>
2025-05-06 02:17:05 +0800

    feat: improve dotnet cards by breaking down the top svg

--- src/components/Section7/Section7.tsx
+++ src/components/Section7/Section7.tsx
@@ -1,13 +1,14 @@
 import { ReactNode } from "react";
-import CardOne from "../../../static/img/Section7/dotnet_cardone.svg";
-import CardTwo from "../../../static/img/Section7/dotnet_cardtwo.svg";
-import CardThree from "../../../static/img/Section7/dotnet_cardthree.svg";
+import CardHead from "../../../static/img/Section7/card_head.svg";
+import Popularity from "../../../static/img/Section7/popularity.svg";
+import Performance from "../../../static/img/Section7/performance.svg";
+import Versatility from "../../../static/img/Section7/versatility.svg";
 
 export default function Section7(): ReactNode {
     return (
-        <section className="bg-[url(/img/Section7/seventh_background.webp)] bg-cover w-screen h-[1171px]">
+        <section className="bg-[url(/img/Section7/seventh_background.webp)] bg-cover h-[1171px]">
             <div className="container !pt-[274.77px] !pb-[152.23px]">
-                <div className="flex flex-col justify-center !pb-[60px]">
+                <div className="flex flex-col justify-center !pb-15">
                     <div className="relative">
                         <h1 className="!text-[56px] text-center leading-[53.76px] tracking-[0.56px] font-semibold  !m-0">
                             <span className="text-white">
@@ -36,8 +37,12 @@ export default function Section7(): ReactNode {
                 <div className="flex justify-between items-center gap-x-[4.94px]">
                     
                     <div className="basis-[402.05px] relative">
-                        <CardOne className="absolute top-0 w-full" />
-                        <div className="bg-[#5438DC] rounded-b-[24px] mt-[99px] pt-[57px] pl-[32px] pr-[46px] pb-[50px]">
+                        <div className="absolute w-full">
+                            <p className="text-[32px] leading-8 absolute top-[15px] left-[19px]">01</p>
+                            <CardHead className="absolute top-0 w-full" />
+                            <Popularity className="absolute top-8 right-[32.05px]" />
+                        </div>
+                        <div className="bg-[#5438DC] rounded-b-[24px] mt-[99px] pt-[57px] pl-8 pr-[46px] pb-[50px]">
                             <h4 className="!text-[40px] leading-[48px] !mb-[38px]">Popularity</h4>
                             <p className="!text-[20px] leading-[28px]">NET Core was ranked as the top <br />
                             non-web framework in the 2024<br />
@@ -49,10 +54,14 @@ export default function Section7(): ReactNode {
                     </div>
                     
                     <div className="basis-[402.05px] relative">
-                        <CardTwo className="absolute top-0 w-full" />
-                        <div className="bg-[#5438DC] rounded-b-[24px] mt-[99px] pt-[57px] pl-[32px] pr-[48.5px] pb-[50px] bg-no-repeat">
-                            <h4 className="!text-[40px] leading-[48px] !mb-[38px]">Performance</h4>
-                            <p className="!text-[20px] leading-[28px]">NET Core delivers high-<br />
+                    <div className="absolute w-full">
+                            <p className="text-[32px] leading-8 absolute top-[15px] left-[19px]">02</p>
+                            <CardHead className="absolute top-0 w-full" />
+                            <Performance className="absolute top-8 right-[32.05px]" />
+                        </div>
+                        <div className="bg-[#5438DC] rounded-b-[24px] mt-[99px] pt-[57px] pl-8 pr-[48.5px] pb-[50px]">
+                            <h4 className="!text-[40px] leading-12 !mb-[38px]">Performance</h4>
+                            <p className="!text-[20px] leading-7">NET Core delivers high-<br />
                             performance applications, with <br />
                             tooling like ASP.NET Core <br />
                             outperforming other popular <br />
@@ -60,11 +69,15 @@ export default function Section7(): ReactNode {
                         </div>
                     </div>
                     
-                    <div className="basis-[402.05px] rounded-b-[24px] relative">
-                        <CardThree className="absolute top-0 w-full" />
-                        <div className="bg-[#5438DC] rounded-b-[24px] mt-[99px] pt-[57px] pl-[32px] pr-[48.5px] pb-[50px]">
-                            <h4 className="!text-[40px] leading-[48px] !mb-[38px]">Versatility</h4>
-                            <p className="!text-[20px] leading-[28px]">.NET supports development <br />
+                    <div className="basis-[402.05px] relative">
+                        <div className="absolute w-full">
+                            <p className="text-[32px] leading-8 absolute top-[15px] left-[19px]">03</p>
+                            <CardHead className="absolute top-0 w-full" />
+                            <Versatility className="absolute top-8 right-[32.05px]" />
+                        </div>
+                        <div className="bg-[#5438DC] rounded-b-[24px] mt-[99px] pt-[57px] pl-8 pr-[48.5px] pb-[50px]">
+                            <h4 className="!text-[40px] leading-12 !mb-[38px]">Versatility</h4>
+                            <p className="!text-[20px] leading-7">.NET supports development <br />
                             across Windows, Linux, and <br />
                             macOS, enabling the creation <br />
                             of applications for desktop, <br />

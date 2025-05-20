93b5ad8b74240fa8497cfb908931e735eb677c9c
KeeeeEf <kfrgallego@gmail.com>
2025-05-20 22:27:42 +0800

    fix: section 1 hero text alignment and background image adjustments

--- src/components/Section1/Section1.tsx
+++ src/components/Section1/Section1.tsx
@@ -19,7 +19,7 @@ export default function Section1(): ReactNode {
         <section id="section-1" style={{ backgroundColor: theme.palette.background.default }} className={`relative grid place-content-center bg-cover ${colorMode === 'dark' ? "bg-[url(/img/background_dark.webp)]" : "bg-[url(/img/background_light.webp)]"} pt-12 md:pt-0 md:min-h-210 md:h-screen`}>
             <div className="container mx-auto text-center">
                 <div>
-                    <h1 className="!text-[40px] sm:!text-[60px] md:!text-[80px] lg:!text-[104px]">
+                    <h1 className="!text-[40px] sm:!text-[60px] md:!text-[80px] sm:leading-14 sm:mb-6 md:leading-18 md:mb-8 lg:!text-[104px] lg:mb-10 lg:leading-24">
                         <span className="inline-flex items-center -translate-y-1 sm:-translate-y-2 md:-translate-y-4 ">
                             <span className="min-w-6 tracking-[-9.62px] leading-[-20px] text-[30px] mr-4 md:mr-6 md:text-[46px] lg:text-[52.88px]" style={{ fontFamily: "Space Mono" }}>
                                 <span> &gt; _</span>

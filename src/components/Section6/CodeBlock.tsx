88551c7a0685de9b965fbb56b87e395f1bd04849
KeeeeEf <kfrgallego@gmail.com>
2025-05-20 20:50:58 +0800

    fix: update code block sample on section 4

--- src/components/Section6/CodeBlock.tsx
+++ src/components/Section6/CodeBlock.tsx
@@ -177,17 +177,30 @@ export default function CodeBlock({ editorRef }) {
       }}
       defaultValue={`public class BlockReducer(IDbContextFactory<AppDbContext> dbContextFactory) : IReducer<BlockData>
 {
-  public async Task RollBackwardAsync(ulong slot)
-  {
-    //RollBack logic here
-  }
+    private readonly string _validatorScriptHash = configuration.GetValue("ValidatorScriptHash", "fda46e81fdd2e4f1c358d27e7484b9f0860b63b0e8e12e6ab5f87e8c");
 
-  public async Task RollForwardAsync(CBlock block)
-  {
-    var blockSlot = block.Header().HeaderBody().Slot();
-    var blockCbor = block.Raw is null ? CborSerializer.Serialize(block) : block.Raw;
-    var blockDataEntry = new BlockData(blockSlot, blockCbor.Value.ToArray());
-  }
+    public async Task RollBackwardAsync(ulong slot)
+    {
+        IQueryable<BlockData> blockDataQuery = dbContext.BlockData
+            .Where(data => data.Slot >= slot);
+
+        dbContext.BlockData.RemoveRange(blockDataQuery);
+        await dbContext.SaveChangesAsync();
+    }
+
+    public async Task RollForwardAsync(Block block)
+    {
+        List<TransactionOutput> outputs = block.TransactionOutputs();
+
+        outputs.ForEach(output =>
+        {
+            Address outputAddress = output.Address();
+            if (outputAddress.ScriptHash() != _validatorScriptHash) return;
+            dbContext.OutputData.Add(output);
+        });
+
+        await dbContext.SaveChangesAsync();
+    }
 }`}
     />
   );

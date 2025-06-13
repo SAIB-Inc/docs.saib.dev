afe311de54a16e87d5042e16032b23b3703dc4b4
CML90 <caitlincebu@hotmail.com>
2025-06-13 20:45:27 +0800

    feat: implement mobile layout

--- src/components/Section9/Section9.tsx
+++ src/components/Section9/Section9.tsx
@@ -7,6 +7,7 @@ import FuturaCodeBlock from "./FuturaCodeBlock";
 import Futura from "@site/static/img/Section9/futura";
 import Code from "../../../static/img/Section9/code.svg";
 import Cardano from "@site/static/img/Section9/cardano";
+import CodePortrait from "@site/static/img/Section9/code_portrait";
 
 export default function Section9(): ReactNode {
     const theme = useTheme();
@@ -23,10 +24,10 @@ export default function Section9(): ReactNode {
     };
     return (
         <section className="w-screen md:py-20">
-            <div className="container !mx-auto">
-                <div className="md:w-full md:mb-5 lg:mb-0">
+            <div className="container !mx-auto !p-11 md:p-0 ">
+                <div className="w-full md:mb-5 lg:mb-0">
                     <Paper
-                        className="md:h-[510px] lg:h-[619.7px]"
+                        className="sm:h-[650px] md:h-[510px] lg:h-[619.7px]"
                         sx={{
                             backgroundImage: "url('/img/Section9/background_big.svg')",
                             backgroundSize: 'cover',
@@ -55,29 +56,45 @@ export default function Section9(): ReactNode {
                             />
                         </div>
                         <div>
-                            <Futura className="absolute  md:!text-[200px] md:top-[270px] md:right-[56px] lg:!text-[225px] lg:top-[170.16px] lg:right-[10px] xl:text-[300px] xl:top-[100px] xl:right-[30px] 2xl:!text-[321px] 2xl:top-[100px] 2xl:right-[30px]" />
+                            <Futura className="absolute !hidden md:!block md:!text-[200px] md:top-[270px] md:right-[56px] lg:!text-[225px] lg:top-[170.16px] lg:right-[10px] xl:text-[300px] xl:top-[100px] xl:right-[30px] 2xl:!text-[321px] 2xl:top-[100px] 2xl:right-[30px]" />
                         </div>
-                        <div className="w-full absolute top-8 left-8">
-                            <h1 className="md:!mb-[-12px] lg:!mb-10 xl:!mb-5 lg:!text-[40px] lg:leading-[41.6px]">
+                        <div className="w-full p-6 flex flex-col md:absolute md:left-8 md:top-8 ">
+                            <h1 className="!text-2xl sm:text-[32px] md:!mb-[-12px] lg:!mb-10lg:!text-[40px] lg:leading-[41.6px] xl:!mb-5 ">
                                 <span style={{ color: theme.palette.primary.main }}>Futura</span>
-                                <span style={{ color: theme.palette.grey.A200 }} className="capitalize">- a domain-specific <br />language built on F#</span>
+                                <span style={{ color: theme.palette.grey.A200 }} className="capitalize">- a domain-specific <br className="hidden md:block"/>language built on F#</span>
                             </h1>
+                            <CodePortrait className="self-center !text-[275px] sm:!text-[500px] md:!hidden" />
                             <Code style={{ width: '650px' }} className="hidden md:block lg:hidden" />
                             <Code style={{ width: '740px' }} className="hidden lg:block 2xl:hidden" />
                             <Code style={{ width: '900px' }} className="hidden 2xl:block" />
                         </div>
                         <div>
-                            <div style={{ borderColor: theme.palette.background.default }} className="absolute border-r-8 border-b-8 w-14 aspect-square rounded-br-3xl md:hidden lg:block lg:scale-80 lg:right-[333px] lg:bottom-[-14px] lg:gap-5 xl:bottom-[-9px] xl:scale-100 xl:right-[387px] 2xl:right-[483px]"></div>
-                            <div style={{ borderColor: theme.palette.background.default, transform: 'rotate(270deg)' }} className="absolute scale-90 top-[-10px] right-[161.58px] border-r-8 border-b-8 w-14 aspect-square rounded-br-3xl lg:"></div>
-                            <div style={{ borderColor: theme.palette.background.default, transform: 'rotate(270deg)' }} className="absolute top-[46px] right-[-8px] border-r-8 border-b-8 w-14 aspect-square rounded-br-3xl lg:border-r-7 lg:border-b-7 lg:w-13"></div>
+                            <div style={{ borderColor: theme.palette.background.default }} className="absolute border-r-8 border-b-8 w-14 aspect-square rounded-br-3xl hidden lg:block lg:scale-80 lg:right-[333px] lg:bottom-[-14px] lg:gap-5 xl:bottom-[-9px] xl:scale-100 xl:right-[387px] 2xl:right-[483px]"></div>
+                            <div style={{ borderColor: theme.palette.background.default, transform: 'rotate(270deg)' }} className="hidden absolute scale-90 top-[-10px] right-[161.58px] border-r-8 border-b-8 w-14 aspect-square rounded-br-3xl md:block"></div>
+                            <div style={{ borderColor: theme.palette.background.default, transform: 'rotate(270deg)' }} className="hidden absolute top-[46px] right-[-8px] border-r-8 border-b-8 w-14 aspect-square rounded-br-3xl lg:block lg:border-r-7 lg:border-b-7 lg:w-13"></div>
                         </div>
                     </Paper>
                 </div>
-
-                <div className="relative flex flex-col-reverse md:flex-col lg:flex-row lg:gap-5">
+                <div className="flex justify-center p-6 md:hidden">
+                            <BtnMore
+                                LinkComponent='a' href="/docs/futura/overview"
+                                sx={{
+                                    color: theme.palette.grey[50],
+                                    '&:hover': {
+                                        backgroundColor: theme.palette.action.hover,
+                                        color: theme.palette.grey[50]
+                                    },
+                                    '&:active': {
+                                        backgroundColor: theme.palette.action.active,
+                                        color: theme.palette.grey[50]
+                                    },
+                                }}
+                            />
+                        </div>
+                <div className="relative flex flex-col-reverse gap-6 md:flex-col lg:flex-row lg:gap-5">
                     <div style={{ backgroundColor: theme.palette.background.default }} className="md:w-full lg:hidden">
                         <Paper
-                            className="p-8"
+                            className="p-6"
                             sx={{
                                 backgroundImage: "url('/img/Section9/background_medium.svg')",
                                 backgroundSize: 'cover',
@@ -88,8 +105,8 @@ export default function Section9(): ReactNode {
                             }}
                         >
                             <div className="flex justify-between">
-                                <div className="flex flex-col justify-end">
-                                    <h1 className="!pb-0 text-2xl sm:text-[32px]">Cardano Smart Contracts</h1>
+                                <div className="flex flex-col justify-end gap-18">
+                                    <h1 className="!pb-0 !text-2xl sm:text-[32px]">Cardano Smart Contracts</h1>
                                     <Button style={{ color: theme.palette.grey[50], borderColor: theme.palette.grey[50] }} variant="outlined" endIcon={<RightArrow />}
                                         sx={{
                                             borderRadius: '60px',
@@ -99,14 +116,14 @@ export default function Section9(): ReactNode {
                                         Get Started
                                     </Button>
                                 </div>
-                                <div>
+                                <div className="flex items-end">
                                     <Cardano className="!text-[93px] md:!text-[174px]"/>
                                 </div>
                             </div>
                         </Paper>
                     </div>
 
-                    <div className="flex flex-col justify-between md:flex-row md:gap-4 pt-5 lg:w-[calc(100%-350px)] lg:gap-5 xl:w-[calc(100%-400px)] 2xl:w-[calc(100%-496px)] ">
+                    <div className="flex flex-col justify-between gap-6 md:flex-row md:gap-4 md:pt-5 lg:w-[calc(100%-350px)] lg:gap-5 xl:w-[calc(100%-400px)] 2xl:w-[calc(100%-496px)] ">
                         <Paper
                             className="flex-1 justify-between p-6 md:p-8"
                             sx={{
@@ -118,7 +135,7 @@ export default function Section9(): ReactNode {
                                 borderRadius: '24px',
                             }}
                         >
-                            <h1 className="font-semibold leading-12 text-2xl mb-8 md:mb-10 lg:mb-22 lg:!text-3xl xl:text-13">Interoperability</h1>
+                            <h1 className="font-semibold leading-12 !text-2xl !mb-8 md:mb-10 lg:mb-22 lg:!text-3xl xl:text-13">Interoperability</h1>
                             <p>
                                 Futura integrates seamlessly with
                                 other .NET languages, enabling smooth
@@ -137,7 +154,7 @@ export default function Section9(): ReactNode {
                                 borderRadius: '24px',
                             }}
                         >
-                            <h1 className="font-semibold leading-12 text-2xl mb-8 md:mb-10 lg:mb-22 lg:!text-3xl xl:text-13">Compile to UPLC</h1>
+                            <h1 className="font-semibold leading-12 !text-2xl !mb-8 md:mb-10 lg:mb-22 lg:!text-3xl xl:text-13">Compile to UPLC</h1>
                             <p>
                                 Futura compiles F# code to UPLC,
                                 unlocking the potential for <b>Cardano</b>
@@ -176,9 +193,7 @@ export default function Section9(): ReactNode {
                                     >
                                         Get Started
                                     </Button>
-
-                                    <Cardano className="absolute mb-4 lg:top-[-170px] lg:right-[4px] lg:!text-[140px] xl:hidden" />
-                                    <Cardano className="hidden absolute mb-4 xl:block xl:top-[-170px] xl:right-[-18px] 2xl:right-[4px]" />
+                                    <Cardano className="absolute mb-4 lg:top-[-170px] lg:right-[4px] lg:!text-[140px] xl:top-[-170px] xl:right-[-18px] 2xl:right-[4px]" />
                                 </div>
                             </div>
                         </Paper>
